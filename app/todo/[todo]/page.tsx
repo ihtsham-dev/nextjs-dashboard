f8b59e14e1d0684b5514999d86a0ee16e5af2155
ihtsham-dev <engineer.js101@gmail.com>
2025-05-15 14:16:30 +0500

    refactor: improve metadata generation and formatting in Todo page component

--- app/todo/[todo]/page.tsx
+++ app/todo/[todo]/page.tsx
@@ -1,28 +1,39 @@
-import type { Metadata, ResolvingMetadata } from 'next'
- 
+import type { Metadata, ResolvingMetadata } from "next";
+
 type Props = {
-  params: Promise<{ todo: string }>
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}
- 
+  params: Promise<{ todo: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
 export async function generateMetadata(
   { params, searchParams }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
-  const slug = (await params).todo
- 
+  const slug = (await params).todo;
+
   // fetch post information
   const post = await fetch(`https://api.vercel.app/blog/${slug}`).then((res) =>
     res.json()
-  )
- 
+  );
+  console.log(post);
+
   return {
     title: post.title,
-    description: post.description,
-  }
+    description: post.content,
+    openGraph: {
+      title: post.title,
+      description: post.content,
+      url: `https://nextjs-dashboard-two-sandy-84.vercel.app/todo/${slug}`,
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post.title,
+      description: post.content,
+    },
+  };
 }
- 
+
 export default function Page({ params, searchParams }: Props) {
-  
-  return <div>dsqwd</div>
+  return <div>dsqwd</div>;
 }
