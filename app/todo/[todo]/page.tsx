b5ba30f50e55df8c1966de8cc73bfe32859b9371
ihtsham-dev <engineer.js101@gmail.com>
2025-05-15 13:48:52 +0500

    fix: update Props interface and simplify metadata fetching in Todo component

--- app/todo/[todo]/page.tsx
+++ app/todo/[todo]/page.tsx
@@ -1,10 +1,11 @@
 import { Photo } from "@/app/lib/definitions";
 import React, { FC } from "react";
+import { Metadata } from "next";
 
 interface Props {
-  params: Promise<{
+  params: {
     todo: string;
-  }>;
+  };
 }
 
 async function getPost(slug: string) {
@@ -16,17 +17,32 @@ async function getPost(slug: string) {
   );
   return res.json();
 }
-async function generateMetadata({ params }: Props) {
-  const post = await getPost((await params).todo);
+
+// This function must be exported
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const post = await getPost(params.todo);
+
   return {
     title: post?.title,
+    // You can add more metadata properties here
+    description: `Details for photo ${post?.title}`,
+    openGraph: {
+      title: post?.title,
+      description: `Details for photo ${post?.title}`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: post?.title,
+      description: `Details for photo ${post?.title}`,
+    },
   };
 }
+
 const Todo: FC<Props> = async ({ params }) => {
-  const { todo } = await params;
+  const { todo } = params;
   const data: Photo = await getPost(todo);
 
-  return <div>Todo:{data?.title}</div>;
+  return <div>Todo: {data?.title}</div>;
 };
 
 export default Todo;
